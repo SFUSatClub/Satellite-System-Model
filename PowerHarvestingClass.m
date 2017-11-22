70243f6c7a4fd737b100a4e8e5114001096577ea
Richard Kwok <richardjkwok@gmail.com>
2017-11-21 23:48:15 -0800

    Extend PowerHarvestingClass with method to calculate instantaneous available solar power

--- PowerHarvestingClass.m
+++ PowerHarvestingClass.m
@@ -1,7 +1,13 @@
 classdef PowerHarvestingClass
+    
+    properties (Constant)
+        solarPanelEfficency = 0.283;     %[]
+        solarPanelSize = 900;            %[cm^2]
+    end
+    
     properties
-        inEclipse;               %boolean
-        solarPanelEfficency;    %m
+        inEclipse; %boolean
+        Wout;      %W (assumes one panel per each of the four sides)
     end
     
     methods
@@ -22,6 +28,39 @@ classdef PowerHarvestingClass
             else
                 PowerObj.inEclipse = 0;
             end
-        end    
+        end
+        
+        function PowerObj = CalculateInstantaneousPower(PowerObj, SunPositionObj, SatPositionObj, AttitudeDCM)
+            
+            if (PowerObj.inEclipse)
+                PowerObj.Wout = 0.0;
+                return;
+            end
+    
+            % The body-centric face vectors of the faces with panels
+            A = [0  1  0]';
+            B = [0 -1  0]';
+            C = [0  0  1]';
+            D = [0  0 -1]';
+            
+            % Attitude-rotated face vectors in ECI
+            Ag = AttitudeDCM*A; 
+            Bg = AttitudeDCM*B;
+            Cg = AttitudeDCM*C;
+            Dg = AttitudeDCM*D;
+            
+            AM0 = 135.3; %[mW/cm^2] Standard value for solar radiation
+            
+            % We assume one identical panel on each of the four sides.
+            RadiationVector = AM0*(SunPositionObj.ECI - SatPositionObj.ECI);
+            
+            Win_A = 1000*dot(Ag, RadiationVector)*solarPanelSize; % W
+            Win_B = 1000*dot(Bg, RadiationVector)*solarPanelSize; % W
+            Win_C = 1000*dot(Cg, RadiationVector)*solarPanelSize; % W
+            Win_D = 1000*dot(Dg, RadiationVector)*solarPanelSize; % W
+            
+            PowerObj.Wout = solarPanelEfficieny*(Win_A + Win_B + Win_C + Win_D);
+            
+        end
     end 
 end
